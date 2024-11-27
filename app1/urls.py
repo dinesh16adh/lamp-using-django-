f05355025899c613255cf7e803c924de652a2785
dineshadhikari01 <dinesh683adhikari@gmail.com>
2024-11-27 15:26:00 +0545

    templates added

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     # path("home/", views.home, name="home"),
-    path("home/<str:user>/", views.home, name="home"),
+    # path("home/<str:user>/", views.home, name="home"),
     # path("welcome_page/<int:user>/", views.welcome_page, name="welcome_page"),
     # path("article/<int:year>/<int:month>", views.article, name="welcome_page"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("home/<user>/", views.home, name="home"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,31 +3,43 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+# def home(request, user):
+
+#     home_page_html = """
+#         <!DOCTYPE html>
+#         <html>
+#         <body>
+
+#         <h1>{{user}}</h1>
+#         <p>my first paragraph.</p>
+#         </body>
+#         </html>
+#     """
+#     home_page_html = home_page_html.replace("{{user}}", user)
+
+#     return HttpResponse(home_page_html)
+
+
+# def welcome_page(requst, user):
+#     message = "welcome to django " + str(user)
+#     return HttpResponse(message)
+
+
+# def article(request, year, month):
+#     if month > 12:
+#         return HttpResponse("this is invalid month format")
+#     return HttpResponse(
+#         f"<h1>this is the year {year} and month {month} from article<h1>"
+#     )
+
+
 def home(request, user):
-
-    home_page_html = """
-        <!DOCTYPE html>
-        <html>
-        <body>
-        
-        <h1>{{user}}</h1>
-        <p>my first paragraph.</p>
-        </body>
-        </html>
-    """
-    home_page_html = home_page_html.replace("{{user}}", user)
-
-    return HttpResponse(home_page_html)
+    return render(request, "app1/index.html")
 
 
-def welcome_page(requst, user):
-    message = "welcome to django " + str(user)
-    return HttpResponse(message)
+def contact(request):
+    pass
 
 
-def article(request, year, month):
-    if month > 12:
-        return HttpResponse("this is invalid month format")
-    return HttpResponse(
-        f"<h1>this is the year {year} and month {month} from article<h1>"
-    )
+def about(request):
+    pass
