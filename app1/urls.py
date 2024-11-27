194dae696ee4236615ea78596ff196065c951264
dineshadhikari01 <dinesh683adhikari@gmail.com>
2024-11-27 13:57:53 +0545

    static urls

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
+    # path("welcome_page/<str:user>/", views.welcome_page, name="welcome_page"),
+    path("welcome_page/<int:user>/", views.welcome_page, name="welcome_page"),
+    path("article/<int:year>/<int:month>", views.article, name="welcome_page"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("hello world")
+
+
+def welcome_page(requst, user):
+    message = "welcome to django " + str(user)
+    return HttpResponse(message)
+
+
+def article(request, year, month):
+    if month > 12:
+        return HttpResponse("this is invalid month format")
+    return HttpResponse(
+        f"<h1>this is the year {year} and month {month} from article<h1>"
+    )
